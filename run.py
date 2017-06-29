57e0211b06b85a5b80999c9313a48e27ca1df018
Whitney Griffith <t-whgrif@microsoft.com>
2017-06-29 02:04:59 -0700

    cancelled previous commit

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ This script runs the FlaskWebProject application using a development server.
 """
 
 from os import environ
-from app import app
+from FlaskWebProject import app
 
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
@@ -11,4 +11,4 @@ if __name__ == '__main__':
         PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
-    app.run(HOST, PORT), debug=True
+    app.run(HOST, PORT)
