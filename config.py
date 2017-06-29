036dc10f98c5ce19af37310f17c063405a78725e
Whitney Griffith <t-whgrif@microsoft.com>
2017-06-29 16:07:28 -0700

    Transferring App to mysql + web app

--- config.py
+++ config.py
@@ -1,3 +1,17 @@
+import os
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'guess'
 
+#Called basedir in tutorial
+DB_PATH = os.path.join(os.path.dirname(__file__), 'microblog.db')
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://yampeep@safepassage-mysqldbserver:sweetpotatoes@safepassage-mysqldbserver.mysql.database.azure.com/safepassage'.format(DB_PATH)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+'''
+Database admin login
+u: admin1234
+pw: @Qwerty1234
+'''
