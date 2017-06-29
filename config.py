ab729cb93429bf50fcc72fe04ef729707af61b5f
Whitney Griffith <t-whgrif@microsoft.com>
2017-06-29 16:27:33 -0700

    Mysql connector URL

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ SECRET_KEY = 'guess'
 
 #Called basedir in tutorial
 DB_PATH = os.path.join(os.path.dirname(__file__), 'microblog.db')
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://yampeep@safepassage-mysqldbserver:sweetpotatoes@safepassage-mysqldbserver.mysql.database.azure.com/safepassage'.format(DB_PATH)
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://admin1234@microblog:@Qwerty1234@microblog.mysql.database.azure.com/microblog'.format(DB_PATH)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
@@ -14,4 +14,8 @@ SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 Database admin login
 u: admin1234
 pw: @Qwerty1234
+
+mysql://username:password#@xxxxxx.hostedresource.com/dbname'
+
+mysql+mysqlconnector://ServerAdminLoginName:password@serverName/dbname
 '''
